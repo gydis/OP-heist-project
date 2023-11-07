3e82be3be2745c735e302aa51db848230e713ecc
Pranav <pranavavarshan.sathishkumar@aalto.fi>
2023-11-07 23:22:18 +0200

    implemented click to return region functionality

--- app_prototype/main.py
+++ app_prototype/main.py
@@ -3,14 +3,11 @@ import plotly.express as px
 import pandas as pd
 import geopandas as gpd
 import streamlit as st
+from streamlit_plotly_events import plotly_events
 
 # Load GeoJSON data
 data = gpd.read_file(r'C:\Users\pvs31\Desktop\Data Science Project\OP-heist-project\map_poly\maptest3.geojson')
 st.set_page_config(layout="wide")
-
-tab_titles = ['Main', 'Sub']
-tabs = st.tabs(tab_titles)
- 
 
 data_source = data
 fig = px.choropleth_mapbox(data_source, geojson=data_source.geometry, locations=data_source.index,
@@ -47,13 +44,13 @@ st.markdown("""
     }
     </style>
 """, unsafe_allow_html=True)
-with tabs[0]:
-    st.plotly_chart(fig,use_container_width=True)
-with tabs[1]:
-    st.title("New Streamlit App")
-    st.write("hi")
 
-
+fig.update_layout(clickmode='event+select')
+#st.plotly_chart(fig,use_container_width=True)
+selected_points = plotly_events(fig)
+first_point = selected_points[0]
+x_value = first_point["pointIndex"]
+st.header(data['nimi'].iloc[int(x_value)])
 
 
 
